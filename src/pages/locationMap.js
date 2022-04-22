07a3068505e7eab38f392b91b8e9f3d526b472bd
Charli <charlisung@gmail.com>
2022-04-22 15:41:14 -0700

    add env file

--- src/pages/locationMap.js
+++ src/pages/locationMap.js
@@ -14,9 +14,12 @@ const position = {
 const onLoad = (marker) => {
   // console.log("marker: ", marker);
 };
+
+const { REACT_APP_MAP_API } = process.env;
+
 function Map() {
   return (
-    <LoadScript googleMapsApiKey="AIzaSyCenPxeDBr4DZi279Pi5O3UmLCPRWEc4ak">
+    <LoadScript googleMapsApiKey={REACT_APP_MAP_API}>
       <GoogleMap mapContainerStyle={containerStyle} center={position} zoom={14}>
         <Marker onLoad={onLoad} position={position} />
       </GoogleMap>
